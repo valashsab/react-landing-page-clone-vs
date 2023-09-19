b5f19c8be4a6461548a3370fef67e718e87e83b1
Val Sab <valerie.sabando@gmail.com>
2023-09-19 22:34:52 +0200

    Copy elements style header, footer, sections

--- src/App.js
+++ src/App.js
@@ -1,12 +1,113 @@
 import styles from './App.module.scss';
-
-// import logo from './logo.svg';
+import childplaying2 from './images/childplayingwithsitter2.svg';
+import childrenplaying1 from './images/childrenplayingwithsitter.svg';
+import logo from './images/otter_logo.png';
 
 export default function App() {
   return (
-    <body className={styles.heroSection}>
-      <nav>
-        <ul>
+    <body className="heroSection">
+      <main>
+        <section>
+          <nav>
+            <ul>
+              <li>
+                <a href="https://withotter.com/parents">Parents</a>
+              </li>
+              <li>
+                <a href="https://withotter.com/sitters">Sitters</a>
+              </li>
+              <li>
+                <a href="https://withotter.com/trust-safety">Trust & Safety</a>
+              </li>
+              <li>
+                <a href="https://withotter.com/blog">Blog</a>
+              </li>
+            </ul>
+
+            <div>
+              <a href="https://withotter.com/">
+                <img src={logo} alt="Otter Logo" />
+              </a>
+            </div>
+
+            <div>
+              <a href="https://app.withotter.com/log-in?device-id=YKWeCmS4b2R5MLCOtW1LF_">
+                Log in
+              </a>
+              <button
+                className={styles.navsignupbtn}
+                onClick="https://app.withotter.com/sign-up/welcome?device-id=YKWeCmS4b2R5MLCOtW1LF_"
+              >
+                Sign up
+              </button>
+            </div>
+          </nav>
+          <div className={styles.imgBackground}>
+            <h1>Reliable kidcare on demand</h1>
+            <p>
+              Otter matches parents who need care with trusted sitters in their
+              community, on-demand.
+            </p>
+            <button className={styles.h1signupbtn}>Sign up</button>
+          </div>
+        </section>
+        <section className={styles.introplaydates}>
+          <img
+            className={styles.childrenplaying1}
+            src={childrenplaying1}
+            alt="children playing with sitter"
+          />
+          <div>NOW IN BETA</div>
+          <h2>Introducing Playdates</h2>
+          <p>
+            Relax and leave the childcare to us. Schedule a playdate with a
+            friend and up to three children at no extra cost.
+          </p>
+          <button
+            className={styles.bookaplaydatebtn}
+            onClick="https://app.withotter.com/log-in"
+          >
+            Book a Playdate
+          </button>
+        </section>
+        <section className={styles.howitworks}>
+          <div>HOW IT WORKS</div>
+          <h2>We help families find reliable, flexible kidcare</h2>
+          <img src={childplaying2} alt="child playing with sitter" />
+        </section>
+
+        <section className={styles.boxes}>
+          <div className={styles.boxOne}>
+            <div>1</div>
+            <h3>Get started</h3>
+            <p>
+              We like to keep it simple. Tell us your name, contact information,
+              and a little bit about you and your kids. Just like that, you're
+              all set.
+            </p>
+          </div>
+
+          <div className={styles.boxTwo}>
+            <div>2</div>
+            <h3>Book a sitter</h3>
+            <p>
+              Let us know when you need care and we'll match you with a sitter
+              based on your family's needs and their availability.
+            </p>
+          </div>
+
+          <div className={styles.boxThree}>
+            <div>3</div>
+            <h3>Get care and pay</h3>
+            <p>
+              Let us know when you need care and we'll match you with a sitter
+              based on your family's needs and their availability.
+            </p>
+          </div>
+        </section>
+      </main>
+      <footer>
+        <ul className={styles.footerColumnOne}>
           <li>
             <a href="https://withotter.com/parents">Parents</a>
           </li>
@@ -15,181 +116,9 @@ export default function App() {
           </li>
           <li>
             <a href="https://withotter.com/trust-safety">Trust & Safety</a>
-          </li>
-          <li>
-            <a href="https://withotter.com/blog">Blog</a>
-          </li>
-          <li>
-            <a href="https://withotter.com/">Logo here</a>
-          </li>
-          <li>
-            <a href="https://app.withotter.com/log-in?device-id=YKWeCmS4b2R5MLCOtW1LF_">
-              Log in
-            </a>
-          </li>
-          <li>
-            <button onClick="https://app.withotter.com/sign-up/welcome?device-id=YKWeCmS4b2R5MLCOtW1LF_">
-              Sign up
-            </button>
           </li>
         </ul>
-      </nav>
-      <h1>Reliable kidcare on demand</h1>
-      <p>
-        Otter matches parents who need care with trusted sitters in their
-        community, on-demand.
-      </p>
-      <button>Sign up</button>
-
-      {/*      <img src="./images/A_person_holding_a_baby" alt="a person holding a baby">
-        a person holding a baby
-      </img>
-
-      <img src="" alt="Kid playing with sitter a">
-        Kid playing with sitter
-      </img>
-
-      <img src="" alt="child with their hands on their head">
-        a child with their hands on their head
-      </img>
-    </>
-  );
-}
- <div>NOW IN BETA</div>
-      <h2>Introducing Playdates</h2>
-      <p>
-        Relax and leave the childcare to us. Schedule a playdate with a friend
-        and up to three children at no extra cost.
-      </p>
-      <button>Book a Playdate</button>
-      <img src="" alt="">
-        A child handing a ball to a smiling caregiver
-      </img>
-      <div>HOW IT WORKS</div>
-      <h2>We help families find reliable, flexible kidcare</h2> <div>1</div>{' '}
-      <h3>Get started</h3>{' '}
-      <p>
-        We like to keep it simple. Tell us your name, contact information, and a
-        little bit about you and your kids. Just like that, you're all set.
-      </p>{' '}
-      <div>2</div> <h3>Book a sitter</h3>{' '}
-      <p>
-        Let us know when you need care and we'll match you with a sitter based
-        on your family's needs and their availability.
-      </p>{' '}
-      <div>3</div> <h3>Get care and pay</h3>{' '}
-      <p>
-        We've got it from here. Your sitter will show up so you can head out.
-        After your booking wraps up you'll receive a payment request through our
-        app.
-      </p>{' '}
-      <h3>We’d love to have you join our Village</h3>
-      <p>
-        Get reliable on demand childcare from vetted sitters in your community
-      </p>
-      .<button>Join Otter </button>
-      <div>TESTIMONIALS </div>
-      <h2>What our Village is saying</h2>{' '}
-      <p>Otter sitters step in when things come up.</p>
-      <p>
-        "I’m very happy with everything and will be booking again soon. My
-        daughter adored her sitter from the first second! (And she was sound
-        asleep when I got home.) It was perfect.” Carmen E, Parent
-      </p>
-      <p>
-        "Otter has been a great way for me to make money on the side while still
-        being incredibly manageable with my busy schedule." Sarah O, Sitter
-      </p>
-      <p>
-        “It was great. Super smooth and easy. We will definitely use you all
-        again!” Mariana S, Parent
-      </p>
-      <p>
-        "Emily got on the floor with our twins as soon as she walked in. She was
-        immediately their new best friend." Casey G, Parent
-      </p>
-      <p>
-        “Otter makes babysitting easy, stress-free, and fun!” Cassy R, Sitter
-      </p>
-      <p>
-        "Both Stephanie and Cassie have been amazing with the girls!! We're so
-        happy with Otter, thank you for all of your great support. " Shelley M,
-        Parent
-      </p>
-      <p>
-        “As a solo parent with no family around, it's been a relief to know I
-        can count on Otter for affordable and quality care in my community.”
-        Carmen E, Parent
-      </p>
-      <p>
-        "Having Cassy care for our kids is something that we look forward to
-        every week. She is everything we could hope for and more in someone who
-        can trust our children with." Laura S, Parent
-      </p>
-      <p>
-        “All the families I've worked with so far have really appreciated Otter
-        and the service we are providing.” Becca D, Sitter
-      </p>
-      <h2>AS FEATURED IN</h2>
-      <img src="" alt="">
-        Forbes magazine
-      </img>
-      <img src="" alt="">
-        parents.com
-      </img>
-      <img src="" alt="">
-        The New York Times
-      </img>
-      <img src="" alt="">
-        mom.com
-      </img>
-      <div>WHY OTTER?</div> <h2>We pop in so you can pop out</h2>
-      <p>Making the impossible things about childcare possible.</p>
-      <h3>Looking Ahead</h3>
-      <p>
-        Look at you being so ahead of the game. We can book childcare up to a
-        month out — we look forward to seeing you then.
-      </p>
-      <img src="" alt="">
-        Mom planning while holding a baby
-      </img>
-      <h3>A Night Out</h3>
-      <p>
-        Nothing should get in the way of the time you spend together, without
-        your kids. Our sitters make date night easy to pull off.
-      </p>
-      <img src="" alt="">
-        Mom planning while holding a baby
-      </img>
-      <h3>In a Pinch</h3>
-      <p>
-        Emergency? Sick nanny? No problem, we can help! We can fill requests
-        with as little as two hours' notice.
-      </p>
-      <h2>Common questions</h2>
-      <button>View all FAQs</button>
-      <h3>What is Otter?</h3>
-      <h3> How is Otter different from other childcare options?</h3>
-      <h3> Does Otter offer last-minute backup care?</h3>
-      <h3> How are Otter's sitters vetted?</h3>
-      <h3>Where is Otter available?</h3>
-      <h2>The bright side of kidcare</h2>
-      <p>
-        Get reliable on demand childcare from vetted sitters in your community.
-      </p>
-        <button>Get started</button> */}
-
-      <footer>
-        <ul>
-          <li>
-            <a href="https://withotter.com/parents">Parents</a>
-          </li>
-          <li>
-            <a href="https://withotter.com/sitters">Sitters</a>
-          </li>
-          <li>
-            <a href="https://withotter.com/trust-safety">Trust & Safety</a>
-          </li>
+        <ul className={styles.footerColumnTwo}>
           <li>
             <a href="https://withotter.com/faq">FAQ</a>
           </li>
@@ -202,6 +131,8 @@ export default function App() {
           <li>
             <a href="https://withotter.com/contact">Contact</a>
           </li>
+        </ul>
+        <ul className={styles.footerColumnThree}>
           <li>
             <a href="https://www.instagram.com/otterchildcare/">Instagram</a>
           </li>
@@ -216,7 +147,7 @@ export default function App() {
           </li>
         </ul>
         Go to home page
-        <ul>
+        <ul className={styles.termsAndPolicy}>
           <li>
             <a href="https://withotter.com/terms-of-use"> Terms of Use</a>
           </li>
@@ -226,7 +157,7 @@ export default function App() {
           <li>
             <a href="https://withotter.com/privacy-policy">Privacy Policy</a>
           </li>
-          <FileList>© 2023 With Otter Inc.</FileList>
+          <li>© 2023 With Otter Inc.</li>
         </ul>
       </footer>
     </body>
